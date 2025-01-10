727c59f403ea785fecb9e79a4d278b58cee541b5
Oudom <limoudom2001@gmail.com>
2025-01-10 22:57:43 +1100

    add frontend url to cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://vocalai.netlify.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
